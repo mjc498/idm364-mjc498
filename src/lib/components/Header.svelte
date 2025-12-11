00a7dc45441c48494ebe2ac8c3c08967a301ae83
Maria-Louisa Ching <mjc498@drexel.edu>
2025-12-10 22:13:48 -0500

    🧼 Cleaning up...

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from '$app/state';
 	import LoginModal from '$lib/components/LoginModal.svelte';
-	import SignUpModal from '$lib/components/SignupModal.svelte';
+	import SignupModal from '$lib/components/SignupModal.svelte';
 	
 	let showLoginModal = $state(false);
 	let showSignUpModal = $state(false);
